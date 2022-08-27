252faf900570902c9a96ed84148390453935ce38
Drazen04 <hangtime23@hotmail.it>
2022-08-27 23:07:15 +0200

    API URL on Constants class.

--- src/main/java/it/stepwise/alfresco/restapiclient/common/Constants.java
+++ src/main/java/it/stepwise/alfresco/restapiclient/common/Constants.java
@@ -1,6 +1,12 @@
 package it.stepwise.alfresco.restapiclient.common;
 
 public class Constants {
+
+    // API URL
+    public static final String BASE_URL_CORE_NODES_API = "alfresco/api/-default-/public/alfresco/versions/" + Constants.NUM_VERSION_PLACEHOLDER + "/nodes/" + Constants.NODE_ID_PLACEHOLDER;
+    public static final String BASE_URL_SEARCH_API = "alfresco/api/-default-/public/search/versions/" + Constants.NUM_VERSION_PLACEHOLDER + "/search";
+
+    // PLACEHOLDERS
     public static final String NUM_VERSION_PLACEHOLDER = "{num_version}";
     public static final String NODE_ID_PLACEHOLDER = "{nodeId}";
 }
