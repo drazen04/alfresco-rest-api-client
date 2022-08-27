f3bd6cebe0b8a8e658e3cd1ba5e48e1c11b70612
Drazen04 <hangtime23@hotmail.it>
2022-08-27 23:06:20 +0200

    test: APIUtilTest.

--- src/test/java/it/stepwise/alfresco/restapiclient/util/APIUtilTest.java
+++ src/test/java/it/stepwise/alfresco/restapiclient/util/APIUtilTest.java
@@ -1,30 +1,23 @@
 package it.stepwise.alfresco.restapiclient.util;
 
-import it.stepwise.alfresco.restapiclient.core.nodes.NodesApi;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+
+import static it.stepwise.alfresco.restapiclient.common.Constants.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class APIUtilTest {
 
     @Test
-    public void when_replace_version_in_url_ok() {
-        String uriWithVersionPlaceholder = NodesApi.BASE_URL_CORE_NODES_API;
-        String uriWithVersionValue = APIUtil.insertVersionInURL(uriWithVersionPlaceholder, 1);
+    public void test_url_composition() {
+        String nodeId = UUID.randomUUID().toString();
+        String URL = APIUtil.composeURL(
+                BASE_URL_CORE_NODES_API,
+                (url) -> url.replace(NUM_VERSION_PLACEHOLDER, "1"),
+                (url) -> url.replace(NODE_ID_PLACEHOLDER, nodeId)
+        );
 
-        String expectedUri = "alfresco/api/-default-/public/alfresco/versions/1/nodes/{nodeId}";
-
-        assertEquals(expectedUri, uriWithVersionValue);
-    }
-
-    @Test
-    public void when_replace_nodeId_in_url_ok() {
-        String uriWithNodeIdPlaceholder = NodesApi.BASE_URL_CORE_NODES_API;
-        String uriWithNodeIdValue = APIUtil.insertNodeIdInURL(uriWithNodeIdPlaceholder, "6efa8f55-59e9-44f6-a3be-a9f1e39a209a");
-
-        System.out.println(uriWithNodeIdValue);
-        String expectedUri = "alfresco/api/-default-/public/alfresco/versions/{num_version}/nodes/6efa8f55-59e9-44f6-a3be-a9f1e39a209a";
-
-        assertEquals(expectedUri, uriWithNodeIdValue);
+        assertEquals("alfresco/api/-default-/public/alfresco/versions/1/nodes/" + nodeId, URL);
     }
 }
