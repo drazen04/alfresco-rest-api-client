ba05d95ca834b1e013de7ec027a9b3144175c603
Drazen04 <hangtime23@hotmail.it>
2022-08-27 23:05:10 +0200

    change httpstatus type: httpstatus -> int.

--- src/main/java/it/stepwise/alfresco/restapiclient/util/Error.java
+++ src/main/java/it/stepwise/alfresco/restapiclient/util/Error.java
@@ -1,20 +1,18 @@
 package it.stepwise.alfresco.restapiclient.util;
 
-import org.apache.http.HttpStatus;
-
 public class Error {
 
-    private final HttpStatus httpStatus;
+    private final int httpStatus;
     private final String error;
     private final String message;
 
-    public Error(HttpStatus httpStatus, String error, String message) {
+    public Error(int httpStatus, String error, String message) {
         this.httpStatus = httpStatus;
         this.error = error;
         this.message = message;
     }
 
-    public HttpStatus getHttpStatus() {
+    public int getHttpStatus() {
         return httpStatus;
     }
 
