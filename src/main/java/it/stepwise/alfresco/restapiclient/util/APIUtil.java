d6cc1f1d17a9eb5b43395fe11950b7f39aa068dc
Lamarcus04 <marco.damiani@stepwise.it>
2022-08-30 18:23:16 +0200

    http: multipart function

--- src/main/java/it/stepwise/alfresco/restapiclient/util/APIUtil.java
+++ src/main/java/it/stepwise/alfresco/restapiclient/util/APIUtil.java
@@ -1,5 +1,13 @@
 package it.stepwise.alfresco.restapiclient.util;
 
+import java.io.IOException;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -8,5 +16,31 @@ public class APIUtil {
     @SafeVarargs
     public static String composeURL(String baseURL, Function<String, String>... fieldToCompose) {
         return Stream.of(fieldToCompose).reduce( Function.identity(), Function::andThen ).apply(baseURL);
+    }
+
+    public static BodyPublisher ofMimeMultipartData(Map<Object, Object> data,
+                                                                String boundary) throws IOException {
+        var byteArrays = new ArrayList<byte[]>();
+        byte[] separator = ("--" + boundary + "\r\nContent-Disposition: form-data; name=")
+                .getBytes(StandardCharsets.UTF_8);
+        for (Map.Entry<Object, Object> entry : data.entrySet()) {
+            byteArrays.add(separator);
+
+            if (entry.getValue() instanceof Path) {
+                var path = (Path) entry.getValue();
+                String mimeType = Files.probeContentType(path);
+                byteArrays.add(("\"" + entry.getKey() + "\"; filename=\"" + path.getFileName()
+                        + "\"\r\nContent-Type: " + mimeType + "\r\n\r\n")
+                        .getBytes(StandardCharsets.UTF_8));
+                byteArrays.add(Files.readAllBytes(path));
+                byteArrays.add("\r\n".getBytes(StandardCharsets.UTF_8));
+            }
+            else {
+                byteArrays.add(("\"" + entry.getKey() + "\"\r\n\r\n" + entry.getValue() + "\r\n")
+                        .getBytes(StandardCharsets.UTF_8));
+            }
+        }
+        byteArrays.add(("--" + boundary + "--\r\n").getBytes(StandardCharsets.UTF_8));
+        return BodyPublishers.ofByteArrays(byteArrays);
     }
 }
