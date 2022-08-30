5aad44293adfa52384470c1c37ccf10426521c60
Lamarcus04 <marco.damiani@stepwise.it>
2022-08-30 18:23:49 +0200

    classic bean to record class.

--- src/main/java/it/stepwise/alfresco/restapiclient/util/Error.java
+++ src/main/java/it/stepwise/alfresco/restapiclient/util/Error.java
@@ -1,26 +1,3 @@
 package it.stepwise.alfresco.restapiclient.util;
 
-public class Error {
-
-    private final int httpStatus;
-    private final String error;
-    private final String message;
-
-    public Error(int httpStatus, String error, String message) {
-        this.httpStatus = httpStatus;
-        this.error = error;
-        this.message = message;
-    }
-
-    public int getHttpStatus() {
-        return httpStatus;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-}
+public record Error(int httpStatus, String error, String message) {}
