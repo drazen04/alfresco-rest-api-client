1e4cbc31a061c0b210e371e64ea2c7f6a949b828
Lamarcus04 <marco.damiani@stepwise.it>
2022-08-30 18:24:24 +0200

    test: remove http status.

--- src/test/java/it/stepwise/alfresco/restapiclient/util/ResponseEitherTest.java
+++ src/test/java/it/stepwise/alfresco/restapiclient/util/ResponseEitherTest.java
@@ -1,17 +1,18 @@
 package it.stepwise.alfresco.restapiclient.util;
 
-import org.apache.http.HttpStatus;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ResponseEitherTest {
 
     @Test
     void handle_error_value() {
+
         // Given
-        ResponseEither<Error, JSONObject> responseEitherWithError = ResponseEither.error(new Error(HttpStatus.SC_BAD_REQUEST, "api not exist", "check for typos"));
+        ResponseEither<Error, JSONObject> responseEitherWithError = ResponseEither.error(new Error(400, "api not exist", "check for typos"));
         ResponseEither<Error, JSONObject> responseEitherNullError = ResponseEither.error(null);
 
         // Then
