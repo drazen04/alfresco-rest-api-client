69f2e5f7baf1c09eeba2f6157ea5e1acc3fdb7f0
Lamarcus04 <marco.damiani@stepwise.it>
2022-09-01 18:54:06 +0200

    utils: add class host.

---
+++ src/main/java/it/stepwise/alfresco/restapiclient/util/Host.java
@@ -0,0 +1,15 @@
+package it.stepwise.alfresco.restapiclient.util;
+
+public class Host {
+    private String hostURL;
+    public Host(String protocol, String domain, int port) {
+        this.hostURL = protocol + "://" + domain + port;
+    }
+    public Host(String protocol, String domain) {
+        this.hostURL = protocol + "://" + domain;
+    }
+
+    public String getHostURL() {
+        return hostURL;
+    }
+}
