760a43df00aa48a3b153d89f7c2969f28838989c
lamarcus <hangtime23@hotmail.it>
2022-09-22 23:36:19 +0200

    add api InputBody

---
+++ src/main/java/it/stepwise/alfresco/restapiclient/InputBody.java
@@ -0,0 +1,20 @@
+package it.stepwise.alfresco.restapiclient;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Field;
+
+public abstract class InputBody {
+
+    public JSONObject toJSON() throws IllegalAccessException {
+        Field[] fields = this.getClass().getDeclaredFields();
+        JSONObject jsonObject = new JSONObject();
+        for (Field field : fields) {
+            field.setAccessible(true);
+            if (field.get(this) != null) {
+                jsonObject.put(field.getName(), field.get(this));
+            }
+        }
+        return jsonObject;
+    }
+}
