04533940e7c0b02e8116a7a1a0b72a428fa84fec
Andrea Fiorucci <andrea.fiorucci@stepwise.it>
2022-11-03 17:59:01 +0100

    fix  "the type parameter is hiding the type" warn

--- src/main/java/it/stepwise/alfresco/restapiclient/util/ResponseEither.java
+++ src/main/java/it/stepwise/alfresco/restapiclient/util/ResponseEither.java
@@ -3,10 +3,10 @@ package it.stepwise.alfresco.restapiclient.util;
 /**
  *
  *
- * @param <Error>
- * @param <R>
+ * @param Errore
+ * @param R
  */
-public class ResponseEither<Error, R> {
+public class ResponseEither<Errore, R> {
 
     private final Error error;
     private final R data;
@@ -16,11 +16,11 @@ public class ResponseEither<Error, R> {
         this.data = data;
     }
 
-    public static <L, R> ResponseEither<L, R> data(R value) {
+    public static <Errore, R> ResponseEither<Errore, R> data(R value) {
         return new ResponseEither<>(null, value);
     }
 
-    public static <Error, R> ResponseEither<Error, R> error(Error value) {
+    public static <Errore, R> ResponseEither<Errore, R> error(Error value) {
         return new ResponseEither<>(value, null);
     }
 
