9fa1c168771b4b0696fd6d1ac82583a487c9de11
lamarcus <hangtime23@hotmail.it>
2023-01-15 18:48:42 +0100

    test: add beforAll to clean node folder before tests.

--- src/test/java/it/stepwise/alfresco/restapiclient/core/nodes/NodesApiTest.java
+++ src/test/java/it/stepwise/alfresco/restapiclient/core/nodes/NodesApiTest.java
@@ -2,6 +2,7 @@ package it.stepwise.alfresco.restapiclient.core.nodes;
 
 import it.stepwise.alfresco.restapiclient.util.ErrorResponse;
 import org.json.JSONObject;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import it.stepwise.alfresco.restapiclient.AlfrescoRestApi;
@@ -12,15 +13,31 @@ import it.stepwise.alfresco.restapiclient.util.ResponseEither;
 
 public class NodesApiTest {
 
-    private Host host = new Host("", "");
-    private AlfrescoRestApi alfrescoRestApi = new AlfrescoRestApi(this.host, "", "");
-    private HttpMethod httpMethod = new HttpMethod(this.alfrescoRestApi);
-    private NodesApi nodesApi = new NodesApi(this.alfrescoRestApi);
+    @BeforeAll
+    public static void delete_test_nodes() {
+        Host host = new Host("http", "localhost");
+        AlfrescoRestApi alfrescoRestApi = new AlfrescoRestApi(host, "admin", "admin");
+
+        NodesApi nodesApi = new NodesApi(alfrescoRestApi);
+
+        // TODO: Remove hard-coded nodes, using SearchApi
+        // TODO: I should implement SitesApi
+        ResponseEither<ErrorResponse, JSONObject> responseEither = nodesApi.deleteNode(
+                "dd23879f-57ab-47e0-914f-eb3df7b24dc9", true);
+        if (!responseEither.hasError()) {
+            ResponseEither<ErrorResponse, JSONObject> createNode = nodesApi.createNode(
+                    "8f2105b4-daaf-4874-9e8a-2152569d109b", new NodeBodyCreate("test", "cm:folder"));
+        }
+    }
 
     @Test
     public void t1_getNode() {
+        Host host = new Host("", "");
+        AlfrescoRestApi alfrescoRestApi = new AlfrescoRestApi(host, "", "");
+        HttpMethod httpMethod = new HttpMethod(alfrescoRestApi);
+        NodesApi nodesApi = new NodesApi(alfrescoRestApi);
 
-        ResponseEither<ErrorResponse, JSONObject> responseEither = this.nodesApi.getNode(
+        ResponseEither<ErrorResponse, JSONObject> responseEither = nodesApi.getNode(
                 "");
 
         System.out.println(responseEither.getData());
@@ -29,9 +46,13 @@ public class NodesApiTest {
     
     @Test
     public void t2_createNode() {
+        Host host = new Host("", "");
+        AlfrescoRestApi alfrescoRestApi = new AlfrescoRestApi(host, "", "");
+        HttpMethod httpMethod = new HttpMethod(alfrescoRestApi);
+        NodesApi nodesApi = new NodesApi(alfrescoRestApi);
 
         NodeBodyCreate nodeBodyCreate = new NodeBodyCreate("", "");
-        ResponseEither<ErrorResponse, JSONObject> responseEither = this.nodesApi.createNode(
+        ResponseEither<ErrorResponse, JSONObject> responseEither = nodesApi.createNode(
                 "",
                 false,
                 nodeBodyCreate,
